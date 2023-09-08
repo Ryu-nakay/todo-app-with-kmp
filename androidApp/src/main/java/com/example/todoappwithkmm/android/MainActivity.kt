36eeacd11ae3a030f8df957b3c2c1d644505082d
Ryu-nakay <ryukenkana@gmail.com>
2023-09-08 21:41:13 +0900

    Android 入力欄のUI作成

--- androidApp/src/main/java/com/example/todoappwithkmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/todoappwithkmm/android/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.todoappwithkmm.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todoappwithkmm.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +28,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    Column {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+
+                            OutlinedTextField(
+                                value = "",
+                                onValueChange = {},
+                                modifier = Modifier.padding(16.dp).height(32.dp)
+                            )
+                            Button(
+                                onClick = { /*TODO*/ }
+                            ) {
+                                Text(text = "追加")
+                            }
+                        }
+
+                        Text(text = "hello world")
+                    }
+
                 }
             }
         }
